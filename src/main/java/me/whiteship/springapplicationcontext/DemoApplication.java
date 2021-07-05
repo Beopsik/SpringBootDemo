72d1724f833b916fcaf68a9beba6f28642cc1381
BeobSik <tjswl756@naver.com>
2021-07-05 15:27:41 +0900

    feat:study spring configure by xml

--- src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
+++ src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
@@ -2,12 +2,19 @@ package me.whiteship.springapplicationcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
+import java.util.Arrays;
+
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext context=new ClassPathXmlApplicationContext("application.xml");
+        String[] beanDefinitionNames=context.getBeanDefinitionNames();
+        System.out.println(Arrays.toString(beanDefinitionNames));
+        BookService bookService=(BookService) context.getBean("bookService");
+        System.out.println(bookService.bookRepository != null);
     }
 
 }
