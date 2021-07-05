501c8309611611e10fc8982ed905d25d1034ee5e
BeobSik <tjswl756@naver.com>
2021-07-05 15:34:54 +0900

    feat:study spring configuration by java configuration file

--- src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
+++ src/main/java/me/whiteship/springapplicationcontext/DemoApplication.java
@@ -3,6 +3,7 @@ package me.whiteship.springapplicationcontext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Arrays;
@@ -10,7 +11,7 @@ import java.util.Arrays;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context=new ClassPathXmlApplicationContext("application.xml");
+        ApplicationContext context=new AnnotationConfigApplicationContext(ApplicationConfig.class);
         String[] beanDefinitionNames=context.getBeanDefinitionNames();
         System.out.println(Arrays.toString(beanDefinitionNames));
         BookService bookService=(BookService) context.getBean("bookService");
