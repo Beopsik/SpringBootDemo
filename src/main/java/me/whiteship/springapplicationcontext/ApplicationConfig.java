b57d939b4f61429cd37a42daf83cd0b8ec88d960
BeobSik <tjswl756@naver.com>
2021-07-05 15:41:18 +0900

    feat:java configuration by componentscan

--- src/main/java/me/whiteship/springapplicationcontext/ApplicationConfig.java
+++ src/main/java/me/whiteship/springapplicationcontext/ApplicationConfig.java
@@ -1,18 +1,10 @@
 package me.whiteship.springapplicationcontext;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
 public class ApplicationConfig {
-    @Bean
-    public  BookRepository bookRepository(){
-        return new BookRepository();
-    }
-
-    @Bean
-    public BookService bookService(){
-        BookService bookService=new BookService();
-        return bookService;
-    }
 }
