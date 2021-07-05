f5ef1a0857f7c3e4a7bfbd9fa152d7e5a3b78e51
BeobSik <tjswl756@naver.com>
2021-07-05 16:42:43 +0900

    feat:use @Autowired with @Qualifier

---
+++ src/main/java/me/whiteship/springapplicationcontext/BeobSikBookRepository.java
@@ -0,0 +1,8 @@
+package me.whiteship.springapplicationcontext;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BeobSikBookRepository implements BookRepository{
+}
--- src/main/java/me/whiteship/springapplicationcontext/BookService.java
+++ src/main/java/me/whiteship/springapplicationcontext/BookService.java
@@ -1,15 +1,17 @@
 package me.whiteship.springapplicationcontext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
+    @Qualifier("beobSikBookRepository")
     @Autowired
     BookRepository bookRepository;
 
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    public void printBookRepository(){
+        System.out.println(bookRepository.getClass());
     }
 }
