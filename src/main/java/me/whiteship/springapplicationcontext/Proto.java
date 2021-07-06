18b24f230aeb15930cef30c70220aba2325ac14f
BeobSik <tjswl756@naver.com>
2021-07-06 14:16:48 +0900

    feat: use ObjectProvider

--- src/main/java/me/whiteship/springapplicationcontext/Proto.java
+++ src/main/java/me/whiteship/springapplicationcontext/Proto.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value = "prototype")
 @Component
 public class Proto {
 }
