c4f3563b5527a163efa44a302fd932f4b286c52a
BeobSik <tjswl756@naver.com>
2021-07-06 15:10:58 +0900

    feat: use test profile with component scan

--- src/main/java/me/whiteship/springapplicationcontext/TestBookRepository.java
+++ src/main/java/me/whiteship/springapplicationcontext/TestBookRepository.java
@@ -1,4 +1,9 @@
 package me.whiteship.springapplicationcontext;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Profile("test")
 public class TestBookRepository implements BookRepository{
 }
