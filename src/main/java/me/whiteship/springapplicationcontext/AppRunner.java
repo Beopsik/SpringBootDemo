ed7f5da4b5958732659b9f4e0974bb0f9e6546d6
BeobSik <tjswl756@naver.com>
2021-07-07 10:14:21 +0900

    feat: ApplicationEventPublisher with async

--- src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
+++ src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
@@ -3,25 +3,17 @@ package me.whiteship.springapplicationcontext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
-
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationEventPublisher publisherEvent;
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        while(true) {
-            System.out.println(messageSource.getMessage("greeting", new String[]{"beobsik"}, Locale.KOREA));
-            System.out.println(messageSource.getMessage("greeting", new String[]{"beobsik"}, Locale.getDefault()));
-            Thread.sleep(1000l);
-        }
+        publisherEvent.publishEvent(new MyEvent(this, 100));
     }
 }
