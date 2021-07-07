e799b2a6656ab83d0a6d4af4cc8d5adba32407bb
BeobSik <tjswl756@naver.com>
2021-07-07 13:08:29 +0900

    feat: EventValidator

--- src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
+++ src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
@@ -1,30 +1,29 @@
 package me.whiteship.springapplicationcontext;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ResourceLoader resourceLoader;
-
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        System.out.println(resourceLoader.getClass());
+        Event event=new Event();
+        EventValidator eventValidator=new EventValidator();
+        Errors errors=new BeanPropertyBindingResult(event, "event");
 
-        Resource resource=resourceLoader.getResource("classpath:test.txt");
-        System.out.println(resource.getClass());
+        eventValidator.validate(event, errors);
 
-        System.out.println(resource.exists());
-        System.out.println(resource.getDescription());
-        System.out.println(Files.readString(Path.of(resource.getURI())));
+        System.out.println(errors.hasErrors());
+        errors.getAllErrors().forEach(e->{
+            System.out.println("---error code---");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
