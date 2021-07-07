f2d398a9ef243dc4b320ca0c76430f762c1c924d
BeobSik <tjswl756@naver.com>
2021-07-07 11:21:24 +0900

    feat: Resource Abstraction

--- src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
+++ src/main/java/me/whiteship/springapplicationcontext/AppRunner.java
@@ -18,7 +18,11 @@ public class AppRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
+        System.out.println(resourceLoader.getClass());
+
         Resource resource=resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
